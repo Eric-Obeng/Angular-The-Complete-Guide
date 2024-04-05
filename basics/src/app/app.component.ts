fcace3f8abf9383a2053e535c4ce531070bb9176
Eric-Obeng <obengeric.3958@gmail.com>
2024-04-05 17:30:27 +0000

    Main component updated

--- basics/src/app/app.component.ts
+++ basics/src/app/app.component.ts
@@ -2,16 +2,17 @@ import { Component } from '@angular/core';
 import { ServerComponent } from './server/server.component';
 import { HeaderComponent } from './header/header.component';
 import { WarningAlertComponent } from './warning-alert/warning-alert.component';
+import { DatabindingComponent } from './databinding/databinding.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [ServerComponent, HeaderComponent, WarningAlertComponent],
+  imports: [ServerComponent, HeaderComponent, WarningAlertComponent, DatabindingComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'basics';
 
-  
+
 }
