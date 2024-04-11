23075e7df695bd695c2cba15c4eb3ba8457d04ee
Eric-Obeng <obengeric.3958@gmail.com>
2024-04-11 01:00:25 +0000

    learnig more on components and databinding

---
+++ cmp-databinding-start/src/app/cockpit/cockpit.component.ts
@@ -0,0 +1,34 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-cockpit',
+  templateUrl: './cockpit.component.html',
+  styleUrl: './cockpit.component.css',
+})
+export class CockpitComponent {
+  @Output() severCreated = new EventEmitter<{
+    serverName: string;
+    serverContent: string;
+  }>();
+  @Output() blueprintCreated = new EventEmitter<{
+    serverName: string;
+    serverContent: string;
+  }>();
+  newServerName = '';
+  newServerContent = '';
+  // serverElements = []
+
+  onAddServer() {
+    this.severCreated.emit({
+      serverName: this.newServerName,
+      serverContent: this.newServerContent,
+    });
+  }
+
+  onAddBlueprint() {
+    this.blueprintCreated.emit({
+      serverName: this.newServerName,
+      serverContent: this.newServerContent,
+    });
+  }
+}
